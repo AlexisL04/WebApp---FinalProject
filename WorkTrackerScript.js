67fa18429a763da92be8617746fc6d916983425b
AlexisL04 <lopeza43@go.stockton.edu>
2025-04-22 14:32:00 -0400

    Made a simple wage calculator web page and a work tracker webapge that will allow users to add their work schedule

---
+++ WorkTrackerScript.js
@@ -0,0 +1,30 @@
+let btnAdd = document.querySelector('button');
+let table = document.querySelector('table');
+
+let titleInput = document.querySelector('#title');
+let startTimeInput = document.querySelector('#startTime');
+let endTimeInput = document.querySelector('#endTime');
+let hoursWorkedInput = document.querySelector('#hoursWorked');
+let notesInput = document.querySelector('#notes');
+
+btnAdd.addEventListener('click', () => {
+
+    let title = titleInput.value;
+    let startTime = startTimeInput.value;
+    let endTime = endTimeInput.value;
+    let hoursWorked = hoursWorkedInput.value;
+    let notes = notesInput.value;
+
+    let template = 
+            `   <tr>
+                    <td>${title}</td>
+                    <td>${startTime}</td>
+                    <td>${endTime}</td>
+                    <td>${hoursWorked}</td>
+                    <td>${notes}</td>
+                </tr>
+                `;
+
+    table.innerHTML += template;
+
+});
